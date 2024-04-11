dac2cd05fc2a5928205b99e1311408e47418a49d
Eric Ross Fu <34407202+rossfu@users.noreply.github.com>
2024-04-11 15:18:29 -0500

    Update website.py

--- website.py
+++ website.py
@@ -42,7 +42,7 @@ st.write(
 
 # Load iris dataset
 iris = load_iris()
-st.write(print(iris.head())
+st.write(print(iris.head()))
 X_train, X_test, y_train, y_test = train_test_split(iris.data, iris.target, test_size=0.2, random_state=42)
 
 # Train Random Forest classifier
