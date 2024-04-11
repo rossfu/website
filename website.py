c5c494b38bf214dc83047593087efe6758192da3
Eric Ross Fu <34407202+rossfu@users.noreply.github.com>
2024-04-10 20:25:41 -0500

    Update website.py

--- website.py
+++ website.py
@@ -4,11 +4,11 @@ st.write("""# Introducing Ross Fu!!""")
 st.write("*Hello World!!!*")
 st.write("*Welcome to my website!!!!*")
 
-
+import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 import seaborn as sns
+import plotly.express as px
 
 # Title
 st.title('Data Science Showcase')
@@ -43,38 +43,25 @@ st.sidebar.subheader('Data Exploration')
 # Scatter plot
 if st.sidebar.checkbox('Show scatter plot'):
     st.subheader('Scatter Plot')
-    plt.figure(figsize=(8, 6))
-    sns.scatterplot(data=data, x='x', y='y')
-    st.pyplot()
+    fig = px.scatter(data, x='x', y='y')
+    st.plotly_chart(fig)
 
 # Histogram
 if st.sidebar.checkbox('Show histogram'):
     st.subheader('Histogram')
-    plt.figure(figsize=(8, 6))
-    sns.histplot(data['x'], bins=20, kde=True)
-    st.pyplot()
+    fig = px.histogram(data, x='x', nbins=20)
+    st.plotly_chart(fig)
 
 # Correlation heatmap
 if st.sidebar.checkbox('Show correlation heatmap'):
     st.subheader('Correlation Heatmap')
     corr = data.corr()
-    plt.figure(figsize=(8, 6))
-    sns.heatmap(corr, annot=True, cmap='coolwarm', linewidths=0.5)
-    st.pyplot()
+    fig = px.imshow(corr, color_continuous_scale='coolwarm')
+    st.plotly_chart(fig)
 
 # Machine Learning
 st.sidebar.title('Machine Learning')
 
 # ML model training
 if st.sidebar.checkbox('Train ML Model'):
-    st.subheader('Train ML Model')
-    # Your machine learning model training code goes here
-
-# Conclusion
-st.sidebar.title('Conclusion')
-st.sidebar.write('Thanks for exploring!')
-
-# About
-st.sidebar.title('About')
-st.sidebar.write('This app is a showcase of my data science abilities.')
-
+    st.subheader('T
