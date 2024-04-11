0882c0546dfbba2ab029fac01ce3b32dfbdb4507
Eric Ross Fu <34407202+rossfu@users.noreply.github.com>
2024-04-11 13:56:10 -0500

    Update website.py

--- website.py
+++ website.py
@@ -31,6 +31,7 @@ with col1:
 
 with col2:
     st.subheader("Data Scientist/Engineer @ Avance Biosciences")
+    st.write("https://www.linkedin.com/in/eric-ross-fu")
     st.write("ericrossfu@yahoo.com")
     st.write("7135404528")
 
