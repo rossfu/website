d5fe7bee4942fddea31b921edf8edea99330d4db
Eric Ross Fu <34407202+rossfu@users.noreply.github.com>
2025-05-21 03:08:51 -0500

    Update website.py

--- website.py
+++ website.py
@@ -44,47 +44,7 @@ with col2:
     st.write("ericrossfu@yahoo.com")
     st.write("713-540-4528")
 
-st.write("")
-st.write("")
-st.write("")
-st.write("Data Visualization Demo")
 
-
-# Generate sample data
-np.random.seed(0)
-n_points = 100
-x = np.random.rand(n_points)
-y = np.random.rand(n_points)
-z = np.random.rand(n_points)
-color = np.random.rand(n_points)
-size = np.random.rand(n_points) * 30
-
-# Create 3D scatter plot
-fig = go.Figure(data=[go.Scatter3d(
-    x=x,
-    y=y,
-    z=z,
-    mode='markers',
-    marker=dict(
-        size=size,
-        color=color,
-        opacity=0.8,
-        colorscale='Viridis'
-    )
-)])
-
-# Customize layout
-fig.update_layout(
-    scene=dict(
-        xaxis_title='X-axis',
-        yaxis_title='Y-axis',
-        zaxis_title='Z-axis'
-    ),
-    margin=dict(l=0, r=0, b=0, t=0)
-)
-
-# Display plot
-st.plotly_chart(fig)
 
 st.write("")
 st.write("")
